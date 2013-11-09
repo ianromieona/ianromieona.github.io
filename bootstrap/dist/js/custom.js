0aa514ce8c04c246e7c9eef63c1705fe037969fa
Ian Romie Ona <ianromie@gmail.com>
2013-11-09 11:57:35 -0800

    revamped

--- bootstrap/dist/js/custom.js
+++ bootstrap/dist/js/custom.js
@@ -1,18 +1,7 @@
 $(document).ready(function(){
 	/* MY AGE */
 	$("#age").text(getAge(new Date(1991, 11, 30)));
-	$("#triggerAbout").click(function(){
-		$("#triggerContact").closest('li').removeClass('active');
-		$("#about").show();
-		$(this).closest('li').addClass('active');
-		$("#contact").hide();
-	})
-	$("#triggerContact").click(function(){
-		 $("#triggerAbout").closest('li').removeClass('active');
-		 $("#about").hide();
-		$(this).closest('li').addClass('active');
-		$("#contact").show();
-	})
+	
 })
 function getAge(d1, d2){
     d2 = d2 || new Date();
